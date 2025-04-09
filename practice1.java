e3cf69dc82048751a12c52a593b5865a6129becc
AdityaPatel369 <137897851+AdityaPatel369@users.noreply.github.com>
2025-04-09 11:30:18 +0530

    calculator

--- practice1.java
+++ practice1.java
@@ -1,11 +1,37 @@
-package college_class_practice;
+// package college_class_practice;
+import java.util.Scanner;
 public class practice1{
     public static void main(String[] args) {
         // 6. Swap two numbers using a temporary variable
 
         System.out.println("PRACTICE SESSION");
- swap_without_temp();
+//  swap_using_temp();
+//  swap_without_temp();  
+// even_odd();
+
+calculator();
+  }
+  public static void calculator(){
+    Scanner input = new Scanner(System.in);
+    System.out.println("enter a and b :");
+    int a = input.nextInt();
+    int b = input.nextInt();
+   System.out.println("To solve : 1.add \n 2.sub ");
+   int c = input.nextInt();
+    switch(c){
+        case 1: 
+        System.out.println("The sum is : ");
+        System.out.print(a+b);
+        break;
+        case 2: 
+        System.out.print("The difference is : ");
+        System.out.print(a-b);
+ default:
+ System.out.println("INVALID INPUT");
     }
+    
+
+  }
 
     public static void swap_using_temp() {
         int a = 12, b = 78;
@@ -18,8 +44,18 @@ public class practice1{
         System.out.println("a is ");
         System.out.println(a);
         System.out.println("b is ");
-            System.out.println(b);
+        System.out.println(b);
 
+    }
+    public static void even_odd (){
+           int a = 3;
+           if(a%2 == 0){
+            System.out.print(a);
+            System.out.println(" is even");
+           }else{
+            System.out.print(a);
+            System.out.println(" is odd");
+           }
     }
 
     public static void swap_without_temp(){
